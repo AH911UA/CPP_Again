3c6dcf0a867169f21b934bc1ba0d38b7818ec323
Aleksandr Hryhorenko <3pervui911@gmail.com>
2019-05-20 13:43:26 +0300

    C R. Day6. array[][]

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 
-using namespace std;
+enum chess
+{
+    empty = 88,
+    pawn = 9,
+    bishop = 6,
+    rook = 5,
+    knight = 3,
+    queen = 2,
+    king = 4
+};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+std::locale::global(std::locale("en_US.UTF-8"));
+    const int arrRow = 8;
+    const int arrCol = 8;
+
+    int shessboard[arrRow][arrCol] =
+    {
+        {bishop, rook, knight, queen, king, knight, rook, bishop},
+        {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
+        {empty, empty, empty, empty, empty, empty, empty, empty},
+        {empty, empty, empty, empty, empty, empty, empty, empty},
+        {empty, empty, empty, empty, empty, empty, empty, empty},
+        {empty, empty, empty, empty, empty, empty, empty, empty},
+        {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
+        {bishop, rook, knight, queen, king, knight, rook, bishop}
+    };
+
+
+    char symb = ' ';
+    for (int i = 0; i < arrRow; i++)
+    {
+        for (int j = 0; j < arrCol; j++)
+        {
+            symb = static_cast<char>(shessboard[i][j]);
+            std::cout << symb  << ' ' ;
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
