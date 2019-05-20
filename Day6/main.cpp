3fe03d721e94bb0e7b83a21913b491c17dedfccb
Aleksandr <32600475+AH911UA@users.noreply.github.com>
2019-05-20 14:53:48 +0300

    Update main.cpp

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 enum chess
 {
@@ -11,34 +12,99 @@ enum chess
     king = 4
 };
 
+//day 6.1
+void ShessFunction();
+
+
+// day 6.2
+
+//Scope
+inline double Figure(double );
+
+//Cylinder
+inline double Figure(double, double);
+
+inline double* GetArr(int);
+
+
+inline void Circle(int, double&);
+
 int main()
 {
-    const int arrRow = 8;
-    const int arrCol = 8;
 
-    int shessboard[arrRow][arrCol] =
+    ShessFunction();
+
+    std::cout << "Scope V : " << Figure(5.5) << std::endl;
+    std::cout << "Cylinder V : " << Figure(5.5, 2.1) << std::endl << std::endl;
+
+    const int arrSize = 5;
+    double *arr = GetArr(arrSize);
+    for(int i = 0; i < arrSize; i ++)
     {
-        {bishop, rook, knight, queen, king, knight, rook, bishop},
-        {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
-        {empty, empty, empty, empty, empty, empty, empty, empty},
-        {empty, empty, empty, empty, empty, empty, empty, empty},
-        {empty, empty, empty, empty, empty, empty, empty, empty},
-        {empty, empty, empty, empty, empty, empty, empty, empty},
-        {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
-        {bishop, rook, knight, queen, king, knight, rook, bishop}
-    };
-
-
-    char symb = ' ';
-    for (int i = 0; i < arrRow; i++)
-    {
-        for (int j = 0; j < arrCol; j++)
-        {
-            symb = static_cast<char>(shessboard[i][j]);
-            std::cout << symb  << ' ' ;
-        }
-        std::cout << std::endl;
+        arr[i] = i * 2;
+        std::cout << arr[i] << ' ';
     }
 
+    std::cout << std::endl << std::endl;
+
+    double res = 0;
+    Circle(4, res);
+    std::cout << "Circle P : " << res;
+
+
     return 0;
+}
+
+
+void ShessFunction()
+{
+    const int arrRow = 8;
+        const int arrCol = 8;
+
+        int shessboard[arrRow][arrCol] =
+        {
+            {bishop, rook, knight, queen, king, knight, rook, bishop},
+            {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
+            {empty, empty, empty, empty, empty, empty, empty, empty},
+            {empty, empty, empty, empty, empty, empty, empty, empty},
+            {empty, empty, empty, empty, empty, empty, empty, empty},
+            {empty, empty, empty, empty, empty, empty, empty, empty},
+            {pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn },
+            {bishop, rook, knight, queen, king, knight, rook, bishop}
+        };
+
+
+        char symb = ' ';
+        for (int i = 0; i < arrRow; i++)
+        {
+            for (int j = 0; j < arrCol; j++)
+            {
+                symb = static_cast<char>(shessboard[i][j]);
+                std::cout << symb  << ' ' ;
+            }
+            std::cout << std::endl;
+        }
+}
+
+
+//Scope
+inline double Figure(double r)
+{
+    return 4 * M_PI * (r * 3);
+}
+//Cylinder
+inline double Figure(double r, double h)
+{
+    return M_PI * (r * 2) * h;
+};
+
+inline double* GetArr(int sizeArr)
+{
+   return new double[sizeArr]();
+}
+
+
+inline void Circle(int r, double &result)
+{
+    result = 2 * M_PI * r;
 }
